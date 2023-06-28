add88d8a80e65a65b4990daab64c838751b70f3c
Nícolas dos Santos Carvalho <nicolas.mnw@gmail.com>
2023-06-27 23:11:44 -0300

    arrumado a porta para deploy com heroku

--- bundle.js
+++ bundle.js
@@ -1093,7 +1093,7 @@ function average(gameNum, oldPercent, percent) {
 
 module.exports = {
     port: 8081,
-    url: "http://18.230.187.254:8081"
+    url: "https://word-api-5975cab08d76.herokuapp.com"
 }
 
 /***/ })
